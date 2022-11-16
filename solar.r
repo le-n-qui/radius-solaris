a681ee1824e98d9f961393ec6e2c91328e13c36e
Andy Le <andy.quile@gmail.com>
2022-11-15 18:22:54 -0800

    Add the file with starting code

---
+++ solar.r
@@ -0,0 +1,38 @@
+# Import dataset from csv file
+data <- read.csv('solar-radiation-updated.csv', header = TRUE, sep = ",")
+head(data,100)
+
+# Data Columns
+UNIX <- data$UNIXTime
+Radiation <- data$Radiation
+Temperature <- data$Temperature
+Pressure <- data$Pressure
+Humidity <- data$Humidity
+WindDirection <- data$WindDirection.Degrees.
+Speed <- data$Speed
+DayNight <- data$Day.1.Night.0
+Month <- data$Month
+Day <- data$Day
+TimeOfDay <- data$TimeOfDay
+TimeSunRise <- data$TimeSunRise
+TimeSunSet <- data$TimeSunSet
+
+# Create Factor for Month Variable
+monthfactor <- factor(Month)
+contrasts(monthfactor) <-contr.treatment(4,base=1)
+contrasts(monthfactor)
+
+# Initial Plots
+plot(Temperature,Radiation)
+plot(Pressure, Radiation)
+plot(Humidity, Radiation)
+plot(WindDirection.Degrees., Radiation)
+plot(Speed, Radiation)
+plot(Day.1.Night.0, Radiation)
+
+# Fit
+fit <- lm(Radiation ~ (Temperature + Pressure + Humidity + WindDirection.Degrees. + Speed) * Day.1.Night.0)
+summary(fit)
+plot(fitted.values(fit), rstudent(fit), xlab = "fitted values", ylab = "Studentized residuals", main = "residual plot")
+qqnorm(rstandard(fit))
+abline(0,1)
