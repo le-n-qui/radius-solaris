256bfae1916fd033b0666066deead160c59d581f
wistrasy <102433442+wistrasy@users.noreply.github.com>
2022-11-17 14:42:26 -0800

    Update solar.r
    Tried some transformations, split data up to look at just daytime points

--- solar.r
+++ solar.r
@@ -1,21 +1,28 @@
 # Import dataset from csv file
 data <- read.csv('solar-radiation-updated.csv', header = TRUE, sep = ",")
-head(data,100)
+#For wilson his file is saved somewhere else
+data <- solar.radiation.updated
+#Create a train/test split
+set.seed(1)
+sample <- sample(c(TRUE,FALSE), nrow(data),replace = TRUE, prob = c(0.8,0.2))
+train <- data[sample,]
+test <- data[!sample,]
+head(train,100)
 #tetschun
 # Data Columns
-UNIX <- data$UNIXTime
-Radiation <- data$Radiation
-Temperature <- data$Temperature
-Pressure <- data$Pressure
-Humidity <- data$Humidity
-WindDirection <- data$WindDirection.Degrees.
-Speed <- data$Speed
-DayNight <- data$Day.1.Night.0
-Month <- data$Month
-Day <- data$Day
-TimeOfDay <- data$TimeOfDay
-TimeSunRise <- data$TimeSunRise
-TimeSunSet <- data$TimeSunSet
+UNIX <- train$UNIXTime
+Radiation <- train$Radiation
+Temperature <- train$Temperature
+Pressure <- train$Pressure
+Humidity <- train$Humidity
+WindDirection <- train$WindDirection.Degrees.
+Speed <- train$Speed
+DayNight <- train$Day.1.Night.0
+Month <- train$Month
+Day <- train$Day
+TimeOfDay <- train$TimeOfDay
+TimeSunRise <- train$TimeSunRise
+TimeSunSet <- train$TimeSunSet
 
 # Create Factor for Month Variable
 monthfactor <- factor(Month)
@@ -31,21 +38,16 @@ plot(Speed, Radiation)
 plot(DayNight, Radiation)
 
 # Fit
-fit <- lm(Radiation ~ (Temperature + Pressure + Humidity + WindDirection.Degrees. + Speed) * Day.1.Night.0)
+fit <- lm(Radiation ~ (Temperature + Pressure + Humidity + WindDirection + Speed) * DayNight)
 summary(fit)
+
+#Residual Assumptions
 plot(fitted.values(fit), rstudent(fit), xlab = "fitted values", ylab = "Studentized residuals", main = "residual plot")
 qqnorm(rstandard(fit))
 abline(0,1)
-set.seed(1)
-sample <- sample(c(TRUE,FALSE), nrow(solar.radiation.updated),replace = TRUE, prob = c(0.8,0.2))
-train <- solar.radiation.updated[sample,]
-test <- solar.radiation.updated[!sample,]
-fit <- lm(Radiation ~ (Temperature + Pressure + Humidity + WindDirection.Degrees. + Speed) * Day.1.Night.0, data = train)
-summary(fit)
-plot(fitted.values(fit), rstudent(fit), xlab = "fitted values", ylab = "Studentized residuals", main = 
-       "residual plot")
-qqnorm(rstandard(fit))
-abline(0,1)
+
+#Initial variable Select
+
 library(leaps)
 ?regsubsets
 all <- regsubsets(x=cbind(solar.radiation.updated$Temperature,solar.radiation.updated$Pressure, solar.radiation.updated$Humidity, solar.radiation.updated$WindDirection.Degrees., solar.radiation.updated$Speed), y=solar.radiation.updated$Radiation,  method = "exhaustive", all.best = FALSE, nbest = 3)
@@ -56,3 +58,27 @@ SSRes <- summary(all)$rss
 R2 <- summary(all)$rsq
 Matrix <- summary(all)$which
 cor(solar.radiation.updated[3:7])
+p <- apply(Matrix,1, sum)
+MSRes <- SSRes/(length(Day)-p)
+output <- cbind(p, Matrix, SSRes, R2, AdjR2, MSRes, Cp)
+colnames(output)[3:7] <- c("Temperature", "Pressure", "Humidity", "Wind Direction", "Wind Speed") 
+output
+
+#Box-Cox
+
+library(MASS)
+boxcox(fit, seq(-1,1,.1))
+
+#Attempt Ln(Radiation) transformation
+fit <- lm(log(Radiation) ~ (Temperature + Pressure + Humidity + WindDirection + Speed) * DayNight)
+summary(fit)
+
+#Remove Night time values
+
+train2 <- train[train$Day.1.Night.0 == 1,]
+train <- train2
+fit <- lm(Radiation ~ Temperature + Pressure + Humidity + WindDirection + Speed)
+summary(fit)
+fit <- lm(log(Radiation) ~ Temperature + Pressure + Humidity + WindDirection + Speed)
+summary(fit)
+
