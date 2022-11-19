01ea9d9123e106b50df44e05ceb29ca820cf9580
wistrasy <wstrasilla@gmail.com>
2022-11-19 11:14:24 -0800

    Changes to Organization
    Changed the organization a little bit. Also made a variable for time since sunset and did some correlation checks

--- solar.r
+++ solar.r
@@ -2,15 +2,17 @@
 # By Multiple Linear Regression
 
 # Import dataset from CSV file
-data <- read.csv('solar-radiation-updated.csv', header = TRUE, sep = ",")
-#For wilson his file is saved somewhere else
-data <- solar.radiation
+data <- read.csv('solar-radiation.csv', header = TRUE, sep = ",")
 
 # Create a train/test split
 set.seed(1)
 sample <- sample(c(TRUE,FALSE), nrow(data), replace = TRUE, prob = c(0.8,0.2))
 train <- data[sample,]
 test <- data[!sample,]
+
+#Remove Night time values
+
+train <- train[train$Day.1.Night.0 == 1,]
 
 # Save data series (columns) 
 # into local variables
@@ -27,22 +29,26 @@ Day <- train$Day
 TimeOfDay <- train$TimeOfDay
 TimeSunRise <- train$TimeSunRise
 TimeSunSet <- train$TimeSunSet
+TimeSinceSunRise <- TimeOfDay - TimeSunRise
 
 # Create Factor for Month Variable
 monthfactor <- factor(Month)
 contrasts(monthfactor) <-contr.treatment(4,base=1)
 contrasts(monthfactor)
 
-# Initial Plots
-plot(Temperature,Radiation)
-plot(Pressure, Radiation)
-plot(Humidity, Radiation)
-plot(WindDirection, Radiation)
-plot(Speed, Radiation)
-plot(DayNight, Radiation)
+#All numeric variable model
 
-# Fit
-fit <- lm(Radiation ~ (Temperature + Pressure + Humidity + WindDirection + Speed) * DayNight)
+fit <- lm(Radiation ~ Temperature + Pressure + Humidity + WindDirection + Speed + UNIX + TimeOfDay + TimeSunRise + TimeSunSet)
+summary(fit)
+cor(train)
+
+#Model without night time points, and Time since surise as the time variable
+fit <- lm(Radiation ~ Temperature + Pressure + Humidity + WindDirection + Speed + TimeSinceSunRise)
+summary(fit)
+cor(cbind(Temperature, Pressure, Humidity, WindDirection, Speed, TimeSinceSunRise))
+
+# Fit with DayNight
+fit <- lm(Radiation ~ (Temperature + Pressure + Humidity + WindDirection + Speed + TimeSinceSunRise) * DayNight)
 summary(fit)
 
 #Residual Assumptions
@@ -78,15 +84,16 @@ boxcox(fit, seq(.4,.6,.1))
 fit <- lm(log(Radiation) ~ (Temperature + Pressure + Humidity + WindDirection + Speed) * DayNight)
 summary(fit)
 
-#Remove Night time values
-
-train2 <- train[train$Day.1.Night.0 == 1,]
-train <- train2
-fit <- lm(Radiation ~ Temperature + Pressure + Humidity + WindDirection + Speed)
-summary(fit)
-
 #Attempting a root transformation on response
 
 fit <- lm(Radiation^.5 ~ Temperature + Pressure + Humidity + WindDirection + Speed)
 summary(fit)
+
+# Initial Plots
+plot(Temperature,Radiation)
+plot(Pressure, Radiation)
+plot(Humidity, Radiation)
+plot(WindDirection, Radiation)
+plot(Speed, Radiation)
+plot(DayNight, Radiation)
 
